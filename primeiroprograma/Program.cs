ff42f14daa7e87349b2802753846182498c46083
Pâmela Borges <pamela_borges18@hotmail.com>
2021-11-04 19:25:46 -0300

    Projeto iniciado mostrando olá mundo

---
+++ primeiroprograma/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace primeiroprograma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá Mundo!");
+        }
+    }
+}
